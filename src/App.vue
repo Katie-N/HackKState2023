fdee26db3246c0628c0ebabbc535d25e2d2bc66d
LucasNordberg <luke.nordberg@gmail.com>
2023-11-04 15:32:40 -0500

    Uploaded full midi files and got keyboard connected successfully

--- src/App.vue
+++ src/App.vue
@@ -21,8 +21,19 @@ export default {
         console.log("WebMidi enabled!");
         console.log(WebMidi.inputs);
         console.log(WebMidi.outputs);
+        this.main();
       })
       .catch(err => alert(err));
+  },
+  methods: {
+    main () {
+      const mySynth = WebMidi.inputs[0];
+      mySynth.channels[1].addListener("noteon", e => {
+        console.log(e.note.name);
+      });
+      console.log(mySynth);
+      
+    },
   }
 }
 </script>
